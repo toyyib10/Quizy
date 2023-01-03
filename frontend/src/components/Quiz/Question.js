1cbd25fcfffeb214c876d177a859936c4e1e35db
Yekeen Toyyib <yekeentoyyib@gmail.com>
2023-01-03 22:42:27 +0100

    i worked on question.js

--- frontend/src/components/Quiz/Question.js
+++ frontend/src/components/Quiz/Question.js
@@ -5,6 +5,15 @@ const Question = () => {
   const [check, setCheck] = useState(false)
   const [change, setChange] = useState(true)
 
+  const answer = (answer) => {
+    setChange(false)
+    setCheck(false)
+    document.getElementById("danger").disabled = true;
+    document.getElementById("info").disabled = true;
+    document.getElementById("warning").disabled = true;
+    document.getElementById("success").disabled = true;
+  }
+
   useEffect(() => {
     const timer = setTimeout(() => { setTime(time - 1) }, 900)
     if (time<6) {
@@ -14,12 +23,17 @@ const Question = () => {
       setChange(false)
       setCheck(false)
       document.getElementById("danger").disabled = true;
-      document.getElementById("primary").disabled = true;
+      document.getElementById("info").disabled = true;
       document.getElementById("warning").disabled = true;
       document.getElementById("success").disabled = true;
       clearTimeout(timer)
     } 
   }, [time])
+
+  useEffect(() => {
+    
+  }, [])
+  
   
   return (
     <>
@@ -35,25 +49,24 @@ const Question = () => {
           </div>
         </section>
         <section className='col-12 col-md-10 col-lg-12 d-flex flex-column align-items-center'>
-          <div className='bg-white p-2 col-9 col-md-8 mb-5 rounded-2 overflow-auto' style={{ "height": "5.6em" }}>
+          <div className='bg-white p-2 ps-3 col-9 col-md-8 mb-5 rounded-2 overflow-auto' style={{ "height": "5.6em" }}>
             <h3 className='overflow-auto'>
               How old are you?
             </h3>
-            
           </div>
           <div className='col-12 d-flex px-3 px-md-4 px-md-4 mb-3 justify-content-between'>
-            <button className='btn btn-danger shadow btn-lg w-50 me-2' id='danger' style={{ "height": "5em" }}>
+            <button onClick={() => answer("A")} className='btn btn-danger shadow btn-lg w-50 me-2' id='danger' style={{ "height": "5em" }}>
               <h2>Elon Musk</h2>
             </button>
-            <button className='btn btn-primary btn-lg shadow ms-2 w-50' id='primary' style={{ "height": "5em" }}>
+            <button onClick={() => answer("B")} className='btn btn-success btn-lg shadow ms-2 w-50 overflow-auto' id='success' style={{ "height": "5em" }}>
               <h2>Bill Gate</h2>
             </button>
           </div>
           <div className='col-12 d-flex px-3 px-md-4 px-md-4 mb-3 justify-content-between'>
-            <button className='btn text-white btn-warning shadow btn-lg w-50 me-2' id='warning' style={{ "height": "5em" }}>
+            <button onClick={() => answer("C")} className='btn text-white btn-warning shadow btn-lg w-50 me-2 overflow-auto' id='warning' style={{ "height": "5em" }}>
               <h2>Jeff Bezos</h2>
             </button>
-            <button className='btn btn-success btn-lg shadow ms-2 w-50' id='success' style={{ "height": "5em" }}>
+            <button onClick={() => answer("D")} className='btn btn-info text-white btn-lg shadow ms-2 w-50 overflow-auto' id='info' style={{ "height": "5em" }}>
               <h2>Yekeen Toyyib</h2>
             </button>
           </div>
